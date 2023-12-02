8e40f90fa5a54461f55ea7ebee767d62f61c1ceb
Brad Davies <brad.davies@withwise.com>
2023-12-02 08:48:50 +0000

    Day 2 - fix test name

--- solutions/d2.test.ts
+++ solutions/d2.test.ts
@@ -19,7 +19,7 @@ describe('d2:p1', () => {
   })
 })
 
-describe('d2:p1', () => {
+describe('d2:p2', () => {
   it('should solve example case', () => {
     const input = `Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
     Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
