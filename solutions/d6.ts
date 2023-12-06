bdb3667a4a6688cf1ef58ffe7be0c904b99abad0
Brad Davies <brad.davies@withwise.com>
2023-12-06 08:24:25 +0000

    Comment

--- solutions/d6.ts
+++ solutions/d6.ts
@@ -2,6 +2,19 @@ import { product } from '../utils/maths'
 
 type Input = { t: number; d: number }
 
+/**
+ * The distance covered forms a quadratic curve of the type `dis = tx - x^2`
+ * Therefore, to find the earliest such point where `dis > d`, we subtract
+ * `d` and end up with an equation like so;
+ * `0 = tx - x^2 - d`
+ *
+ * We then simply need the lowest root of this quadratic equation, which
+ * we use the standard formula for (with some optimisations as `a` is
+ * always `1` in this case).
+ *
+ * We then round up to the nearest number, and subtract 2 times this (as the
+ * curve is symmetrical) and add 1 (as we want the range inclusive).
+ */
 const solve = ({ t, d }: Input) =>
   t - Math.ceil((t - Math.sqrt(t ** 2 - 4 * (d + 1))) / 2) * 2 + 1
 
