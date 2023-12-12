fca65dbd10230bf451b373e33d37822133d2af52
Brad Davies <brad.davies@withwise.com>
2023-12-12 09:04:24 +0000

    Day 12 - Move the memoized variant calculator fn inside the 'solver' invocation, as otherwise the benchmarks are invalid (as the cache is preserved between runs, which gives a slow initial run, followed by many super fast runs (and heavily skews the benchmarks)

--- solutions/d12.ts
+++ solutions/d12.ts
@@ -4,48 +4,48 @@ import { sum } from 'lodash/fp'
 import { loopSum } from '../utils/loop'
 import { memoize } from '../utils/memo'
 
-/**
- * Memoize this function, because if the input is the same (row & group), the answer
- * will always be the same. Plus, we see the same row/group combinations a lot, especially
- * as this solution is _recursive_ so we will never repeat any work we have already seen.
- */
-const calculateVariants = memoize((row: string, groups: number[]): number => {
-  // Skip over any leading dots
-  if (row[0] === '.') return calculateVariants(row.slice(1), groups)
+export const solver = (input: string, n: number) => {
+  /**
+   * Memoize this function, because if the input is the same (row & group), the answer
+   * will always be the same. Plus, we see the same row/group combinations a lot, especially
+   * as this solution is _recursive_ so we will never repeat any work we have already seen.
+   */
+  const calculateVariants = memoize((row: string, groups: number[]): number => {
+    // Skip over any leading dots
+    if (row[0] === '.') return calculateVariants(row.slice(1), groups)
 
-  // If we have no groups left, check if we have any #s left
-  // If we do, then we have an invalid group, so return 0
-  if (groups.length === 0) {
-    return row.indexOf('#') === -1 ? 1 : 0
-  }
+    // If we have no groups left, check if we have any #s left
+    // If we do, then we have an invalid group, so return 0
+    if (groups.length === 0) {
+      return row.indexOf('#') === -1 ? 1 : 0
+    }
 
-  // Check if we can even fit the groups in the row
-  if (row.length < sum(groups) + groups.length - 1) {
-    return 0
-  }
-
-  // If our next item is a #, then try and fit it to a group
-  if (row[0] === '#') {
-    const [groupSize, ...remainingGroups] = groups
-    // If we have a . in the next 'groupSize' spots, we have an invalid group
-    if (row.slice(0, groupSize).indexOf('.') !== -1) return 0
-    // If we have a # at the end of the grouping, it would mean the group is too big, so we have an invalid group
-    if (row[groupSize] === '#') {
+    // Check if we can even fit the groups in the row
+    if (row.length < sum(groups) + groups.length - 1) {
       return 0
     }
-    // Otherwise we have found a valid group, so we can skip over the group and continue to the next one
-    return calculateVariants(row.slice(groupSize + 1), remainingGroups)
-  }
 
-  // We must have a ? as the leading character, so we will replace it with a `#` and a `.` and continue
-  const nextSlice = row.slice(1)
-  return (
-    calculateVariants('#' + nextSlice, groups) +
-    calculateVariants('.' + nextSlice, groups)
-  )
-})
+    // If our next item is a #, then try and fit it to a group
+    if (row[0] === '#') {
+      const [groupSize, ...remainingGroups] = groups
+      // If we have a . in the next 'groupSize' spots, we have an invalid group
+      if (row.slice(0, groupSize).indexOf('.') !== -1) return 0
+      // If we have a # at the end of the grouping, it would mean the group is too big, so we have an invalid group
+      if (row[groupSize] === '#') {
+        return 0
+      }
+      // Otherwise we have found a valid group, so we can skip over the group and continue to the next one
+      return calculateVariants(row.slice(groupSize + 1), remainingGroups)
+    }
 
-export const solver = (input: string, n: number) => {
+    // We must have a ? as the leading character, so we will replace it with a `#` and a `.` and continue
+    const nextSlice = row.slice(1)
+    return (
+      calculateVariants('#' + nextSlice, groups) +
+      calculateVariants('.' + nextSlice, groups)
+    )
+  })
+
   return loopSum((line) => {
     const [str, nums] = line.trim().split(' ')
     const record = Array.from({ length: n }).fill(str).join('?')
